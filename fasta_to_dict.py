1ae358fc375729c6a275d88744091fc26c6e1ad2
Alice <alicescarpa@gmail.com>
2014-10-04 15:08:50 -0300

    Refactored reading code into a function

--- fasta_to_dict.py
+++ fasta_to_dict.py
@@ -1,18 +1,19 @@
 #this will take Fasta format DNA strings and put into a dictionary
 import sys
 
-label = ""
-dna = ""
-dic = {}
+def fasta_to_dict():
+    label = ""
+    dna = ""
+    dic = {}
 
-for tmp in sys.stdin.readlines():
-    line = tmp.rstrip()
-    if line[0] == ">":
-        if label != "":
-            dic[label] = dna
-        dna = ""    
-        label = line[1:]
-    else:
-        dna += line
+    for tmp in sys.stdin.readlines():
+        line = tmp.rstrip()
+        if line[0] == ">":
+            if label != "":
+                dic[label] = dna
+                dna = ""    
+                label = line[1:]
+            else:
+                dna += line
 
-dic[label] = dna
+    dic[label] = dna
